110ee042ccbc6fd07500423f4278ecefaf77620c
greteh <73522688+greteh@users.noreply.github.com>
2022-11-22 18:53:32 +0200

    Update KulmkappDao.kt

--- app/src/main/java/com/example/kulmkapp/room/KulmkappDao.kt
+++ app/src/main/java/com/example/kulmkapp/room/KulmkappDao.kt
@@ -7,20 +7,36 @@ import androidx.room.Query
 
 @Dao
 interface KulmkappDao {
+
+    // Ingredients
+
+    @Query("SELECT * FROM Ingredients")
+    fun loadAllIngredients(): List<IngredientEntity>
+
     @Query("SELECT * FROM Ingredients WHERE id==:ingredientId")
     fun loadSingleIngredientById(ingredientId: Int): IngredientEntity
 
     @Query("SELECT * FROM Ingredients WHERE name like :ingredientName || '%'")
-    fun loadIngredientsLikeName(ingredientName: String): IngredientEntity
+    fun loadIngredientsLikeName(ingredientName: String): List<IngredientEntity>
 
-    @Query("SELECT * FROM Ingredients")
-    fun loadAllIngredients(): IngredientEntity
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertIngredient(vararg ingredient: IngredientEntity)
+
+    // KulmkappItems
+
+    @Query("SELECT * FROM KulmkappItems")
+    fun loadAllKulmkappItems(): List<IngredientEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertKulmkappItem(vararg kulmkappItem: KulmkappItemEntity)
 
+    // Recipes
+
+    @Query("SELECT * FROM Recipes")
+    fun loadAllRecipes(): List<RecipeEntity>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertIngredient(vararg ingredient: IngredientEntity)
+    fun insertRecipe(vararg recipe: RecipeEntity)
 
 
 
