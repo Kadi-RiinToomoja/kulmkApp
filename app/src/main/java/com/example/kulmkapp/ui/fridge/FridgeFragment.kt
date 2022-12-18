1df37ad0b9ff59d35744566825beda90297a9e4c
Pihla Järv <pihla.jarv@gmail.com>
2022-12-18 16:29:55 +0200

    imported necessary thing and now everything ok

--- app/src/main/java/com/example/kulmkapp/ui/fridge/FridgeFragment.kt
+++ app/src/main/java/com/example/kulmkapp/ui/fridge/FridgeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.kulmkapp.ui.fridge
 
 import android.icu.text.SimpleDateFormat
+
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kulmkapp.R
