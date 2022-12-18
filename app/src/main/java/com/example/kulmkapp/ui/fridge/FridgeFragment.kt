f08935ad3d6a92267f386580da3685bf3ee3885d
markussulg <markus7sulg@gmail.com>
2022-12-18 16:32:28 +0200

    fix

--- app/src/main/java/com/example/kulmkapp/ui/fridge/FridgeFragment.kt
+++ app/src/main/java/com/example/kulmkapp/ui/fridge/FridgeFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kulmkapp.R
